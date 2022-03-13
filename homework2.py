f32252813fc2b35194d5e74cb0150d13b7dd58a7
Yeom <ydh7219@naver.com>
2022-03-14 00:17:41 +0900

    homework2.5

--- homework2.py
+++ homework2.py
@@ -1,12 +1,17 @@
 """
-Project: Homework2.4
+Project: Homework2.5
 Author: 염동환
 Date of last update: March 13, 2022
 Update list:
     -v1.0: March 13, 2022
 """
-print("===============================")
-print("Decimal  Bit   Octal Hexadecimal")
-print("-------------------------------")
-for i in range(0,256):
-    print("{0:6d} {1:08b} {2:#05o} {3:#04X}".format(i,i,i,i))
+a_b=list(map(int,input("input a and b : ").split()))
+a=a_b[0]
+b=a_b[1]
+
+print("a({:3d}) + b({:3d}) = {}".format(a,b,a+b))
+print("a({:3d}) - b({:3d}) = {}".format(a,b,a-b))
+print("a({:3d}) * b({:3d}) = {}".format(a,b,a*b))
+print("a({:3d}) / b({:3d}) = {:.6f}".format(a,b,a/b))
+print("a({:3d}) // b({:3d}) = {:.6f}".format(a,b,a//b))
+print("a({:3d}) % b({:3d}) = {:.6f}".format(a,b,a%b))
