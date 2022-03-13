718adb01a22c9869f34b54653b01c559ce5202eb
Yeom <ydh7219@naver.com>
2022-03-13 20:08:26 +0900

    fisrt commit

---
+++ homework1.py
@@ -0,0 +1,7 @@
+from cmath import pi
+import math
+radius = float(input())
+
+area=pi*(radius**2)
+circumference = 2*pi*radius
+print("넓이",area,"\n둘레", circumference)
---
+++ practice.py
@@ -0,0 +1,5 @@
+def perm(data,i,n):
+    if i==n:
+        print(data)
+    else:
+        for i in range(i,n+1)
