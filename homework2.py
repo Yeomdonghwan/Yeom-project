5f48babb20bda8387c96c770c261e6ce845a0a8c
Yeom <ydh7219@naver.com>
2022-03-13 22:46:07 +0900

    homework2.4

--- homework2.py
+++ homework2.py
@@ -1,13 +1,12 @@
 """
-Project: Homework2.2
+Project: Homework2.4
 Author: 염동환
 Date of last update: March 13, 2022
 Update list:
     -v1.0: March 13, 2022
 """
-width_length=list(map(int,input("width, length = ").split()))
-width = width_length[0]
-length = width_length[1]
-area = width*length
-perimeter = 2*width + 2*length
-print("Rectangle of width({}) and length({}) : area ({}), perimeter({})".format(width,length,area,perimeter))
+print("===============================")
+print("Decimal  Bit   Octal Hexadecimal")
+print("-------------------------------")
+for i in range(0,256):
+    print("{0:6d} {1:08b} {2:#05o} {3:#04X}".format(i,i,i,i))
