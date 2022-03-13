34c5bf551fd68845f25b9f4836679ccd9d5dab3b
Yeom <ydh7219@naver.com>
2022-03-13 20:17:03 +0900

    second commit

--- 21912205.py
+++ 21912205.py
@@ -1,19 +1,10 @@
-import turtle
-import math
-from math import pi
+"""
+Project: Homework2.1
+Author: 염동환
+Date of last update: March 13, 2022
+Update list:
+    - v1.0: March 13, 2022
+"""
 
-base=float(input("밑변: "))
-height=float(input("높이: "))
-
-angle=math.atan(2*height/base)*180/pi
-hypotenuse=((height**2)+((base/2)**2))**0.5
-
-t=turtle.Turtle()
-t.forward(base)
-t.left(180-angle)
-t.forward(hypotenuse)
-t.left(2*angle)
-t.forward(hypotenuse)
-
-
-a=input()
+radius = int(input())
+area = 
